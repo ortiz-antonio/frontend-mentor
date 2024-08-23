12cc4d67a362adf64afcc62319ebf63e7b0bac37
Antonio Ortiz <ortiz.antonio@gmail.com>
2024-08-22 22:22:24 -0500

    added qr-code and social-links to monorepo

--- vite-config.js
+++ vite-config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'huddle-landing/index.html'),
+        // nested: resolve(__dirname, 'huddle-landing/index.html'),
       },
     },
   },
